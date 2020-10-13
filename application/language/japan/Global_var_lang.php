7565bc376b9121150d9d82d566101424745cd914
alif firdi <halo@aliffirdi.me>
2020-10-13 10:05:43 +0700

    fix bug & add user settings

---
+++ application/language/japan/Global_var_lang.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//GLOBAL VAR//
+$lang['copyright']					= '&copy;';
+//END GLOBAL VAR//
+
+//form login//
+$lang['login']						= 'ログインする';
+$lang['login_username']				= 'ユーザーID';
+$lang['login_pass']					= 'パスワード';
+$lang['login_forgot_pass']			= 'パスワードを忘れた？';
+$lang['login_remember_me']			= 'セッションを保存';
+$lang['login_punya_akun']			= 'アカウントをお持ちではありませんか？';
+$lang['login_buat_akun']			= 'アカウントを作成する';
+$lang['login_login_by']				= 'でログイン';
+$lang['login_fill_email']			= 'ユーザー名を挿入';
+$lang['login_fill_pass']			= 'パスワードを挿入';
+$lang['login_alert']				= '注意';
+$lang['login_pass_wrong_msg']		= 'ユーザー名かパスワードが間違っています！。';
+$lang['login_belum_login']			= '最初にログインしてください！。';
+$lang['login_after_logout']			= 'ログアウト';
+//end form login//
+
+/* End of file Global_var_lang.php */
+/* Location: ./application/language/Global_var_lang.php */
---
+++ system/language/japan/global_var_lang.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//GLOBAL VAR//
+$lang['copyright']					= '&copy;';
+//END GLOBAL VAR//
+
+//form login//
+$lang['login']						= 'ログインする';
+$lang['login_username']				= 'ユーザーID';
+$lang['login_pass']					= 'パスワード';
+$lang['login_forgot_pass']			= 'パスワードを忘れた？';
+$lang['login_remember_me']			= 'セッションを保存';
+$lang['login_punya_akun']			= 'アカウントをお持ちではありませんか？';
+$lang['login_buat_akun']			= 'アカウントを作成する';
+$lang['login_login_by']				= 'でログイン';
+$lang['login_fill_email']			= 'ユーザー名を挿入';
+$lang['login_fill_pass']			= 'パスワードを挿入';
+$lang['login_alert']				= '注意';
+$lang['login_pass_wrong_msg']		= 'ユーザー名かパスワードが間違っています！。';
+$lang['login_belum_login']			= '最初にログインしてください！。';
+$lang['login_after_logout']			= 'ログアウト';
+//end form login//
+
+//form register//
+$lang['register']					= '登録';
+$lang['register_first_name']		= 'ファーストネーム';
+$lang['register_last_name']			= '苗字';
+$lang['register_username']			= 'ユーザーID';
+$lang['register_email']				= 'Eメール';
+$lang['register_y_country']			= 'あなたの国';
+$lang['register_country']			= '国家';
+$lang['register_prov']				= '県';
+$lang['register_agree']				= '利用規約に同意します';
+//end form register//
+
+/* End of file global_var_lang.php */
+/* Location: ./system/language/global_var_lang.php */
